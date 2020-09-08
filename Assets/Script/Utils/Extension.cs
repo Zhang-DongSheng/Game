9edc26d7ab85ba09dac8fdceace5d4304be52833
dongsheng <zhangdongsheng@branchesinteractive.com>
2020-09-08 19:53:05 +0800

    扩展方法

--- Assets/Script/Utils/Extension.cs
+++ Assets/Script/Utils/Extension.cs
@@ -1,8 +1,46 @@
-﻿using System.Collections;
+﻿using Data;
+using System;
 using System.Collections.Generic;
-using UnityEngine;
 
 public static class Extension
 {
-    
+    #region Dictionary
+    public static V GetByIndex<K, V>(this Dictionary<K, V> pairs, int index)
+    {
+        if (index > -1 && pairs.Count > index)
+        {
+            foreach (KeyValuePair<K, V> pair in pairs)
+            {
+                if (index-- == 0)
+                {
+                    return pair.Value;
+                }
+            }
+        }
+        return default;
+    }
+    #endregion
+
+    #region Number
+    public static string NumberToString(this int value, int digit)
+    {
+        float number;
+
+        string unit = string.Empty;
+
+        if (value < 1000)
+        {
+            number = value;
+        }
+        else if (value < 1000000)
+        {
+            number = value / 1000; unit = "k+";
+        }
+        else
+        {
+            number = value / 1000000; unit = "m+";
+        }
+        return string.Format("{0}{1}", Math.Round(number, digit), unit);
+    }
+    #endregion
 }
