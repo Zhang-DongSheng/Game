acf30d992694a03746ad9d4fe5cf0162a6b7a030
dongsheng <zhangdongsheng@branchesinteractive.com>
2020-09-10 18:48:07 +0800

    本地时间工具

---
+++ Assets/Script/Utils/TimeUtils.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Globalization;
+
+public static class TimeUtils
+{
+    public static DateTime Now
+    {
+        get
+        {
+            return DateTime.Now;
+        }
+    }
+
+    public static DateTime UtcNow
+    {
+        get
+        {
+            return DateTime.UtcNow;
+        }
+    }
+
+    public static DateTime LocalToUtc(DateTime time)
+    { 
+        return time - TimeZoneInfo.Local.BaseUtcOffset;
+    }
+
+    public static DateTime UtcToLocal(DateTime time)
+    {
+        return time + TimeZoneInfo.Local.BaseUtcOffset;
+    }
+
+    public static double TotalSeconds(DateTime begin, DateTime end)
+    {
+        TimeSpan span = end - begin;
+
+        return span.TotalSeconds;
+    }
+
+    public static DateTime ToDateTime(long ticket)
+    {
+        return new DateTime(ticket);
+    }
+
+    public static DateTime ToDateTime(string time)
+    {
+        DateTimeFormatInfo format = new DateTimeFormatInfo()
+        {
+            ShortDatePattern = "yyyy-MM-dd",
+        };
+        return Convert.ToDateTime(time, format);
+    }
+
+    public static string ToString(DateTime time)
+    {
+        return time.ToString();
+    }
+}
