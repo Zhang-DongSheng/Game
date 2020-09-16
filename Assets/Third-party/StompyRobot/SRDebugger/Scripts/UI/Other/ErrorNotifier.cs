da5ea5b5a0be394ba092989d7e3de50ef8111a75
dongsheng <zhangdongsheng@branchesinteractive.com>
2020-09-16 15:42:18 +0800

    SRDebugger

---
+++ Assets/Third-party/StompyRobot/SRDebugger/Scripts/UI/Other/ErrorNotifier.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace SRDebugger.UI.Other
+{
+    public class ErrorNotifier : MonoBehaviour
+    {
+        public bool IsVisible
+        {
+            get { return enabled; }
+        }
+
+        private const float DisplayTime = 6;
+
+        [SerializeField]
+        private Animator _animator = null;
+
+        private int _triggerHash;
+
+        private float _hideTime;
+
+        void Awake()
+        {
+            _triggerHash = Animator.StringToHash("Display");
+            enabled = false;
+        }
+
+        public void ShowErrorWarning()
+        {
+            _hideTime = Time.realtimeSinceStartup + DisplayTime;
+
+            if (!enabled)
+            {
+                enabled = true;
+                _animator.SetBool(_triggerHash, true);
+            }
+        }
+
+        void Update()
+        {
+            if (Time.realtimeSinceStartup > _hideTime)
+            {
+                enabled = false;
+                _animator.SetBool(_triggerHash, false);
+            }
+        }
+    }
+}
