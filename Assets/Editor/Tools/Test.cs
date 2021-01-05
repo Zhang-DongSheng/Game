235e8fcc2e6b68fd1add6cc0052d2c1fb5e11afa
dongsheng <zhangdongsheng@branchesinteractive.com>
2021-01-05 15:42:06 +0800

    排序

---
+++ Assets/Editor/Tools/Test.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using static SortUtils;
+
+namespace TEST
+{
+    public class Test : MonoBehaviour
+    {
+        [MenuItem("Test/OnClick")]
+        private static void OnClick()
+        {
+
+            List<SortStruct> sorts = new List<SortStruct>();
+
+            sorts.Add(new SortStruct() { ID = 1003 });
+            sorts.Add(new SortStruct() { ID = 1001 });
+            sorts.Add(new SortStruct() { ID = 1030 });
+            sorts.Add(new SortStruct() { ID = 1002 });
+            sorts.Add(new SortStruct() { ID = 1900 });
+            sorts.Add(new SortStruct() { ID = 1050 });
+            sorts.Add(new SortStruct() { ID = 1200 });
+            sorts.Add(new SortStruct() { ID = 1300 });
+            sorts.Add(new SortStruct() { ID = 1030 });
+            sorts.Add(new SortStruct() { ID = 1070 });
+            sorts.Add(new SortStruct() { ID = 1001 });
+
+            List<SortStruct> list = SortUtils.QuickSort(sorts);
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Debug.LogError(list[i].ID);
+            }
+        }
+    }
+}
