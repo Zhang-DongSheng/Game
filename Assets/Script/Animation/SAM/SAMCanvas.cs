7f067b3b58151bb87671129f97fbbe2596dc3577
dongsheng <zhangdongsheng@branchesinteractive.com>
2021-01-06 17:16:04 +0800

    alpha动画

---
+++ Assets/Script/Animation/SAM/SAMCanvas.cs
@@ -0,0 +1,40 @@
+﻿namespace UnityEngine.SAM
+{
+    [RequireComponent(typeof(CanvasGroup))]
+    public class SAMCanvas : SAMBase
+    {
+        [SerializeField] private CanvasGroup canvas;
+
+        [SerializeField] private float origin, destination;
+
+        protected override void Awake()
+        {
+            if (canvas == null)
+                canvas = GetComponent<CanvasGroup>();
+        }
+
+        protected override void Renovate()
+        {
+            if (status == SAMStatus.Transition)
+            {
+                step += speed * Time.deltaTime;
+
+                Transition(forward ? step : 1 - step);
+
+                if (step >= SAMConfig.ONE)
+                {
+                    Completed();
+                }
+            }
+        }
+
+        protected override void Transition(float step)
+        {
+            if (canvas == null) return;
+
+            progress = curve.Evaluate(step);
+
+            canvas.alpha = Mathf.Lerp(origin, destination, progress);
+        }
+    }
+}
--- Assets/Script/Animation/SAM/SAMSize.cs
+++ Assets/Script/Animation/SAM/SAMSize.cs
@@ -1,46 +1,46 @@
-﻿using UnityEngine;
-using UnityEngine.SAM;
-
-public class SAMSize : SAMBase
+﻿namespace UnityEngine.SAM
 {
-    [SerializeField] private SAMAxis axis;
-
-    [SerializeField] private Vector2 origin, destination;
-
-    protected override void Renovate()
+    public class SAMSize : SAMBase
     {
-        if (status == SAMStatus.Transition)
+        [SerializeField] private SAMAxis axis;
+
+        [SerializeField] private Vector2 origin, destination;
+
+        protected override void Renovate()
         {
-            step += speed * Time.deltaTime;
-
-            Transition(forward ? step : 1 - step);
-
-            if (step >= SAMConfig.ONE)
+            if (status == SAMStatus.Transition)
             {
-                Completed();
+                step += speed * Time.deltaTime;
+
+                Transition(forward ? step : 1 - step);
+
+                if (step >= SAMConfig.ONE)
+                {
+                    Completed();
+                }
             }
         }
-    }
 
-    protected override void Transition(float step)
-    {
-        if (target == null) return;
-
-        progress = curve.Evaluate(step);
-
-        vector = Vector2.Lerp(origin, destination, progress);
-
-        switch (axis)
+        protected override void Transition(float step)
         {
-            case SAMAxis.Horizontal:
-                target.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, vector.x);
-                break;
-            case SAMAxis.Vertical:
-                target.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, vector.y);
-                break;
-            default:
-                target.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, vector.x);
-                goto case SAMAxis.Vertical;
+            if (target == null) return;
+
+            progress = curve.Evaluate(step);
+
+            vector = Vector2.Lerp(origin, destination, progress);
+
+            switch (axis)
+            {
+                case SAMAxis.Horizontal:
+                    target.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, vector.x);
+                    break;
+                case SAMAxis.Vertical:
+                    target.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, vector.y);
+                    break;
+                default:
+                    target.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, vector.x);
+                    goto case SAMAxis.Vertical;
+            }
         }
     }
 }
