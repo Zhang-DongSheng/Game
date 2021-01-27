290e0561a3214ac53e0d7dc79db166ae12364f52
dongsheng <do_she_ng@163.com>
2021-01-27 10:47:31 +0800

    图文混排

--- Assets/Script/Test/Test.cs
+++ Assets/Script/Test/Test.cs
@@ -6,57 +6,21 @@ namespace Game
 {
     public class Test : MonoBehaviour
     {
-        public RenewableImage img_test;
-
-        public RenewableAsset asset_test;
-
-        public RenewableAudio audio_test;
-
-        public Button btn_test;
-
-        public InputField input_test;
-
-        private string variable;
-
-        private readonly List<string> urls = new List<string>()
-    {
-        "picture/cover/ceo.jpg",
-        "picture/cover/ourhb.jpg",
-    };
+        public Text text;
 
         private void Awake()
         {
-            if (btn_test != null)
-            {
-                btn_test.onClick.AddListener(OnClickTest);
-            }
 
-            if (input_test != null)
-            {
-                input_test.onEndEdit.AddListener(OnSubmit);
-            }
         }
 
         private void Start()
         {
-
+            text.SetTextWithEmoji("AAAABCD<emoji=walking0001/>HLH<emoji=walking0002/>TTTTTTTAAAABCD<emoji=walking0001/>HLH<emoji=walking0002/>zzz");
         }
 
         private void Update()
         {
 
-        }
-
-        private void OnClickTest()
-        {
-
-        }
-
-        Sprite sprite;
-
-        private void OnSubmit(string value)
-        {
-            variable = value;
         }
     }
 }
