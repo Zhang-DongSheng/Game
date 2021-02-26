e1c0571e340459c922c61f614d10536248d3f095
dongsheng <do_she_ng@163.com>
2021-02-26 10:55:42 +0800

    引入新的版本控制工具 解决类库冲突

---
+++ Assets/Script/UI/Widget/UIWaiting.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.UI
+{
+    public class UIWaiting : MonoBehaviour
+    {
+        [SerializeField] private Transform icon;
+
+        [SerializeField, Range(0, 100f)] private float angle = 1f;
+
+        [SerializeField] private bool forward;
+
+        private Vector3 vector;
+
+        private void Awake()
+        {
+            float ratio = forward ? angle : angle * -1f;
+
+            vector = Vector3.forward * ratio;
+        }
+
+        private void OnValidate()
+        {
+            float ratio = forward ? angle : angle * -1f;
+
+            vector = Vector3.forward * ratio;
+        }
+
+        private void Update()
+        {
+            icon.Rotate(vector * Time.deltaTime);
+        }
+
+        public void SetActive(bool active)
+        {
+            if (gameObject != null && gameObject.activeSelf != active)
+            {
+                gameObject.SetActive(active);
+            }
+        }
+    }
+}
