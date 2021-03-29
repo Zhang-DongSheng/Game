301525f589b8b135c57adbd21824b06f7ce99e53
dongsheng <do_she_ng@163.com>
2021-03-29 19:58:11 +0800

    方法

--- Assets/Script/Extension/ExtensionNumber.cs
+++ Assets/Script/Extension/ExtensionNumber.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 
 public static partial class NumberExtension
 {
+    private static double value;
+
     public static int Between(this int self, int min, int max)
     {
         if (self < min)
@@ -42,33 +44,37 @@ public static partial class NumberExtension
         return self;
     }
 
+    public static bool Inside(this Vector2 origin, Vector2 destination, float distance)
+    {
+        value = Math.Pow(origin.x - destination.x, 2) + Math.Pow(origin.y - destination.y, 2);
+
+        return Math.Pow(distance, 2) >= value;
+    }
+
+    public static bool Inside(this Vector3 origin, Vector3 destination, float distance)
+    {
+        value = Math.Pow(origin.x - destination.x, 2) + Math.Pow(origin.y - destination.y, 2) + Math.Pow(origin.z - destination.z, 2);
+
+        return Math.Pow(distance, 2) >= value;
+    }
+
     public static float Distance(this float origin, float destination)
     {
         return Math.Abs(origin - destination);
     }
 
-    public static float Distance(this Vector2 origin, Vector2 destination, bool simple = true)
+    public static float Distance(this Vector2 origin, Vector2 destination)
     {
-        if (simple)
-        {
-            return Math.Abs(origin.x - destination.x) + Math.Abs(origin.y - destination.y);
-        }
-        else
-        {
-            return Vector2.Distance(origin, destination);
-        }
+        value = Math.Pow(origin.x - destination.x, 2) + Math.Pow(origin.y - destination.y, 2);
+
+        return (float)value;
     }
 
-    public static float Distance(this Vector3 origin, Vector3 destination, bool simple = true)
+    public static float Distance(this Vector3 origin, Vector3 destination)
     {
-        if (simple)
-        {
-            return Math.Abs(origin.x - destination.x) + Math.Abs(origin.y - destination.y) + Math.Abs(origin.z - destination.z);
-        }
-        else
-        {
-            return Vector3.Distance(origin, destination);
-        }
+        value = Math.Pow(origin.x - destination.x, 2) + Math.Pow(origin.y - destination.y, 2) + Math.Pow(origin.z - destination.z, 2);
+
+        return (float)value;
     }
 
     public static string ToString(this int self, int digit)
@@ -92,7 +98,7 @@ public static partial class NumberExtension
         return string.Format("{0}{1}", Math.Round(number, digit), unit);
     }
 
-    public static string ToStringTime(this float second)
+    public static string ToDateTime(this float second)
     {
         TimeSpan span = TimeSpan.FromSeconds(second);
 
