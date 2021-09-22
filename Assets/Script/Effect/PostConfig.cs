1b0771dce00f6924c3be8ef3e86ef59808464f38
DoShe <do_she_ng@163.com>
2021-09-22 11:30:01 +0800

    条件检测

--- Assets/Script/Effect/PostConfig.cs
+++ Assets/Script/Effect/PostConfig.cs
@@ -34,6 +34,8 @@ namespace Game.Effect
 
         public void Procession(ref Material material)
         {
+            if (string.IsNullOrEmpty(key)) return;
+
             switch (type)
             {
                 case PostPropertyType.Float:
