24eea2600e80e9df9b5fea3e5d298ccbf5264f63
DoShe <do_she_ng@163.com>
2021-09-28 15:52:17 +0800

    重命名

--- Assets/Script/Test/EditorHelper.cs
+++ Assets/Script/Test/EditorHelper.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Game.Model
+namespace Game
 {
-    public class Glass : ObjectBase
+    public class EditorHelper : MonoBehaviour
     {
         
     }
