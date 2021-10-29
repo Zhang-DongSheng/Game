08cb6bc3b372c57056fa7ded25e5a56b4d634d67
DoShe <do_she_ng@163.com>
2021-10-29 11:32:37 +0800

    准备 Agent 环境，并解决和原有项目的冲突

---
+++ Assets/Script/Study/Agent/LearnAgent.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.MLAgents;
+using UnityEngine;
+
+namespace Game.Learn
+{
+    public class LearnAgent : Agent
+    {
+        
+    }
+}
