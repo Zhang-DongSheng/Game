9eb9023692f23d400c1b93ef4db8a0f5e5dda27b
DoShe <do_she_ng@163.com>
2021-11-26 15:56:42 +0800

    Log工具

---
+++ Assets/Script/Camera/CameraShake.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using UnityEngine;
+
+namespace Game
+{
+    public class CameraShake : MonoBehaviour
+    {
+        [SerializeField] private ShakePattern shake;
+
+        [SerializeField] private float intensityMin = 0.1f;
+
+        [SerializeField] private float intensityMax = 0.5f;
+
+        [SerializeField] private float decay = 0.02f;
+
+        private Vector3 position;
+
+        private Quaternion rotation;
+
+        private Coroutine coroutine;
+
+        private void OnDisable()
+        {
+            if (coroutine != null)
+            {
+                coroutine = null;
+            }
+        }
+
+        public void Shake()
+        {
+            if (coroutine != null) return;
+
+            coroutine = StartCoroutine(ProcessShake());
+        }
+
+        IEnumerator ProcessShake()
+        {
+            position = transform.position;
+
+            rotation = transform.rotation;
+
+            float intensity = Random.Range(intensityMin, intensityMax);
+
+            while (intensity > 0)
+            {
+                switch (shake)
+                {
+                    case ShakePattern.Translate:
+                        transform.position = position + Random.insideUnitSphere * intensity;
+                        break;
+                    case ShakePattern.Rotate:
+                        {
+                            transform.rotation = new Quaternion(rotation.x + Random.Range(-intensity, intensity) * Time.deltaTime,
+                                                                rotation.y + Random.Range(-intensity, intensity) * Time.deltaTime,
+                                                                rotation.z + Random.Range(-intensity, intensity) * Time.deltaTime,
+                                                                rotation.w + Random.Range(-intensity, intensity) * Time.deltaTime);
+                        }
+                        break;
+                    case ShakePattern.TranslateAndRotate:
+                        {
+                            goto case ShakePattern.Translate | ShakePattern.Rotate;
+                        }
+                }
+                intensity -= decay * Time.deltaTime;
+            }
+            yield return null;
+        }
+        [ContextMenu("Debug")]
+        protected void Debug()
+        {
+            Shake();
+        }
+    }
+
+    enum ShakePattern
+    { 
+        None,
+        Translate,
+        Rotate,
+        TranslateAndRotate,
+    }
+}
