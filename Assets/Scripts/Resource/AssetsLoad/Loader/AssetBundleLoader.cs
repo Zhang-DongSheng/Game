9d913a60fc2f5dbbf4fc74947f7b84b876e148a8
DoShe <do_she_ng@163.com>
2022-04-11 18:27:27 +0800

    修改

--- Assets/Scripts/Resource/AssetsLoad/Loader/AssetBundleLoader.cs
+++ Assets/Scripts/Resource/AssetsLoad/Loader/AssetBundleLoader.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Game.Resource
@@ -9,12 +8,40 @@ namespace Game.Resource
     {
         public override AssetsData LoadAssets(string path)
         {
-            throw new NotImplementedException();
+            if (Application.platform == RuntimePlatform.Android)
+            {
+                path = path.Replace(@"jar:file://", "");
+                path = path.Replace("apk!/assets", "apk!assets");
+            }
+            AssetBundle bundel = AssetBundle.LoadFromFile(path);
+
+            if (bundel == null) return null;
+
+            return new AssetsData(path)
+            {
+                AssetBundle = bundel,
+                Assets = bundel.LoadAllAssets(),
+            };
         }
 
         public override IEnumerator LoadAssetsAsync(string path, Action<AssetsData> callback)
         {
-            throw new NotImplementedException();
+            AssetBundleCreateRequest create = AssetBundle.LoadFromFileAsync(path);
+
+            yield return create;
+
+            AssetBundle bundel = create.assetBundle;
+
+            AssetBundleRequest request = bundel.LoadAllAssetsAsync();
+
+            yield return request;
+
+            callback?.Invoke(new AssetsData(path)
+            {
+                AssetBundle = bundel,
+                Assets = request.allAssets,
+            });
+            yield return null;
         }
     }
 }
