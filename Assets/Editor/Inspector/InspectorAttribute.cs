1df22d51a5295b7e07be781450a7ff26989cc253
DoShe <do_she_ng@163.com>
2022-06-07 10:37:24 +0800

    参数类型描述

--- Assets/Editor/Inspector/InspectorAttribute.cs
+++ Assets/Editor/Inspector/InspectorAttribute.cs
@@ -187,6 +187,8 @@ namespace UnityEditor.Inspector
     [CustomPropertyDrawer(typeof(ButtonAttribute))]
     class ButtonDrawer : PropertyDrawer
     {
+        private string function;
+
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
             return EditorGUI.GetPropertyHeight(property, label, true) * 2 + 5;
@@ -206,7 +208,15 @@ namespace UnityEditor.Inspector
 
             position.y += 3;
 
-            if (GUI.Button(position, _attribute.function))
+            if (_attribute.parameter)
+            {
+                function = string.Format("{0}({1} parameter)", _attribute.function, property.type);
+            }
+            else
+            {
+                function = _attribute.function;
+            }
+            if (GUI.Button(position, function))
             {
                 _attribute.Call(property.serializedObject.targetObject, PropertyUtils.Parameter(property));
             }
--- Assets/Scripts/Utility/Utility.Compress.cs
+++ Assets/Scripts/Utility/Utility.Compress.cs
@@ -8,143 +8,24 @@ namespace Game
     {
         public static class _Compress
         {
-            private const int Length = 0x1000;
-
-            private static readonly byte[] buffer = new byte[Length];
-
-            public static bool Compress(byte[] bytes, int offset, int length, Stream stream)
+            public static bool Compress(byte[] bytes, string output)
             {
-                if (bytes == null || stream == null)
-                {
-                    return false;
-                }
-                if (offset < 0 || length < 0 || offset + length > bytes.Length)
-                {
-                    return false;
-                }
-
-                try
-                {
-                    GZipOutputStream gZipOutputStream = new GZipOutputStream(stream);
-                    gZipOutputStream.Write(bytes, offset, length);
-                    gZipOutputStream.Finish();
-                    ProcessHeader(stream);
-                    return true;
-                }
-                catch
-                {
-                    return false;
-                }
+                return false;
             }
 
-            public static bool Compress(Stream input, Stream output)
+            public static bool Compress(string input, string output)
             {
-                if (input == null || output == null)
-                {
-                    return false;
-                }
-
-                try
-                {
-                    GZipOutputStream gZipOutputStream = new GZipOutputStream(output);
-                    int bytesRead = 0;
-                    while ((bytesRead = input.Read(buffer, 0, Length)) > 0)
-                    {
-                        gZipOutputStream.Write(buffer, 0, bytesRead);
-                    }
-
-                    gZipOutputStream.Finish();
-                    ProcessHeader(output);
-                    return true;
-                }
-                catch
-                {
-                    return false;
-                }
-                finally
-                {
-                    Array.Clear(buffer, 0, Length);
-                }
+                return false;
             }
 
-            public static bool Decompress(byte[] bytes, int offset, int length, Stream stream)
+            public static bool Decompress(byte[] bytes, string output)
             {
-                if (bytes == null || stream == null)
-                {
-                    return false;
-                }
-                if (offset < 0 || length < 0 || offset + length > bytes.Length)
-                {
-                    return false;
-                }
-
-                MemoryStream memoryStream = null;
-                try
-                {
-                    memoryStream = new MemoryStream(bytes, offset, length, false);
-                    using (GZipInputStream gZipInputStream = new GZipInputStream(memoryStream))
-                    {
-                        int bytesRead = 0;
-                        while ((bytesRead = gZipInputStream.Read(buffer, 0, Length)) > 0)
-                        {
-                            stream.Write(buffer, 0, bytesRead);
-                        }
-                    }
-
-                    return true;
-                }
-                catch
-                {
-                    return false;
-                }
-                finally
-                {
-                    if (memoryStream != null)
-                    {
-                        memoryStream.Dispose();
-                    }
-                    Array.Clear(buffer, 0, Length);
-                }
+                return false;
             }
 
-            public static bool Decompress(Stream input, Stream output)
+            public static bool Decompress(string input, string output)
             {
-                if (input == null || output == null)
-                {
-                    return false;
-                }
-                try
-                {
-                    GZipInputStream gZipInputStream = new GZipInputStream(input);
-                    int bytesRead = 0;
-                    while ((bytesRead = gZipInputStream.Read(buffer, 0, Length)) > 0)
-                    {
-                        output.Write(buffer, 0, bytesRead);
-                    }
-                    return true;
-                }
-                catch
-                {
-                    return false;
-                }
-                finally
-                {
-                    Array.Clear(buffer, 0, Length);
-                }
-            }
-
-            private static void ProcessHeader(Stream stream)
-            {
-                if (stream.Length >= 8L)
-                {
-                    long current = stream.Position;
-                    stream.Position = 4L;
-                    stream.WriteByte(25);
-                    stream.WriteByte(134);
-                    stream.WriteByte(2);
-                    stream.WriteByte(32);
-                    stream.Position = current;
-                }
+                return false;
             }
         }
     }
