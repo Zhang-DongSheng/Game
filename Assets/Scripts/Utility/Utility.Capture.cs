92d3bbe44083b5f7ef9ab83fc0c12e37b0ddade4
dongsheng <do_she_ng@163.com>
2023-03-13 11:50:48 +0800

    屏幕捕获

---
+++ Assets/Scripts/Utility/Utility.Capture.cs
@@ -0,0 +1,82 @@
+using System;
+using System.IO;
+using UnityEngine;
+
+namespace Game
+{
+    public static partial class Utility
+    {
+        /// <summary>
+        /// 屏幕捕获
+        /// </summary>
+        public class Capture
+        {
+            public static Texture2D Screenshot()
+            {
+                return ScreenCapture.CaptureScreenshotAsTexture();
+            }
+
+            public static Texture2D Screenshot(Rect rect, TextureFormat format = TextureFormat.RGB24)
+            {
+                Texture2D texture = new Texture2D((int)rect.width, (int)rect.height, format, false);
+
+                texture.ReadPixels(rect, 0, 0);
+
+                texture.Apply();
+
+                return texture;
+            }
+
+            public static Texture2D Screenshot(Camera camera, Rect rect, TextureFormat format = TextureFormat.RGB24, bool onetime = false)
+            {
+                if (camera == null) return null;
+
+                if (camera.targetTexture == null)
+                {
+                    camera.targetTexture = new RenderTexture((int)rect.width, (int)rect.height, 0);
+                }
+                camera.Render();
+
+                RenderTexture.active = camera.targetTexture;
+
+                Texture2D texture = new Texture2D(camera.targetTexture.width, camera.targetTexture.height, format, false);
+
+                texture.ReadPixels(rect, 0, 0);
+
+                texture.Apply();
+
+                if (onetime)
+                {
+                    if (camera.targetTexture != null)
+                    {
+                        UnityEngine.GameObject.Destroy(camera.targetTexture);
+                    }
+                    camera.targetTexture = null;
+
+                    RenderTexture.active = null;
+                }
+                return texture;
+            }
+
+            public static void CreatePng(Texture2D texture, string path)
+            {
+                if (texture == null) return;
+
+                try
+                {
+                    byte[] buffer = texture.EncodeToPNG();
+
+                    string direction = Path.GetDirectoryName(path);
+
+                    Document.CreateDirectory(direction);
+
+                    File.WriteAllBytes(path, buffer);
+                }
+                catch (Exception e)
+                {
+                    Debuger.LogException(Author.File, e);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/Capture/CaptureScreen.cs
@@ -0,0 +1,122 @@
+#if UNITY_ANDROID
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game
+{
+    /// <summary>
+    /// б╪фа
+    /// </summary>
+    public class CaptureScreen : MonoBehaviour
+    {
+        [SerializeField] private RawImage image;
+
+        [SerializeField] private RectInt rect;
+
+        private byte[] buffer;
+
+        private bool over;
+
+        private Thread thread;
+
+        private Texture2D texture;
+
+        private void Start()
+        {
+            thread = new Thread(new ThreadStart(Capture))
+            {
+                IsBackground = true,
+            };
+            thread.Start();
+        }
+
+        private void Update()
+        {
+            if (buffer != null && over)
+            {
+                Create(buffer);
+            }
+        }
+
+        private void Capture()
+        {
+            try
+            {
+                while (true)
+                {
+                    if (!over)
+                    {
+                        using (MemoryStream memory = new MemoryStream())
+                        {
+                            Compress(Screen(), memory);
+
+                            buffer = memory.ToArray();
+
+                            over = true;
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+            }
+        }
+
+        private Bitmap Screen()
+        {
+            Bitmap bitmap = new Bitmap(rect.width, rect.height);
+
+            System.Drawing.Graphics graphics = System.Drawing.Graphics.FromImage(bitmap);
+
+            graphics.CopyFromScreen(rect.x, rect.y, 0, 0, bitmap.Size);
+
+            return bitmap;
+        }
+
+        private void Compress(Bitmap bitmap, MemoryStream memory)
+        {
+            try
+            {
+                bitmap.Save(memory, ImageFormat.Jpeg);
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+            }
+        }
+
+        private void Create(byte[] buffer)
+        {
+            if (texture == null)
+            {
+                texture = new Texture2D(rect.width, rect.height);
+            }
+            texture.LoadImage(buffer);
+
+            image.texture = texture;
+
+            image.SetNativeSize();
+
+            over = false;
+        }
+
+        private void OnDestroy()
+        {
+            if (thread != null)
+            {
+                thread.Abort(); thread = null;
+            }
+            if (texture != null)
+            {
+                GameObject.Destroy(texture); texture = null;
+            }
+        }
+    }
+}
+#endif
