0b4771918230bbdcadad29972cc829fe197b69c0
dongsheng <do_she_ng@163.com>
2023-10-09 16:21:46 +0800

    编辑器降级

--- BuildPlayer.bat
+++ BuildPlayer.bat
@@ -1,4 +1,4 @@
-SET UNITY_PATH="C:\Program Files\Unity\Hub\Editor\2022.2.17f1c1\Editor\Unity.exe"
+SET UNITY_PATH="C:\Program Files\Unity 2021.3.26f1\Editor\Unity.exe"
 SET PROJECT_PATH="D:\Project\Game"
 
 %UNITY_PATH% -nographics -batchMode -projectPath %PROJECT_PATH% -executeMethod MonoHook.Test.BuildPipeline_StripDll_HookTest.BuildPlayer -quit -logFile %PROJECT_PATH%\logs\build_player.log
