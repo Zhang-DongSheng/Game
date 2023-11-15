0fbd9ab60009d2e2ef85d0309323f3e5d875d09b
dongsheng <do_she_ng@163.com>
2023-11-15 11:49:48 +0800

    修改

--- Assets/Scripts/Utility/Utility.String.cs
+++ Assets/Scripts/Utility/Utility.String.cs
@@ -12,17 +12,21 @@ namespace Game
         {
             private static readonly StringBuilder builder = new StringBuilder();
 
+            private static int code;
+
             public static float GetInt(string content)
             {
                 builder.Clear();
 
                 foreach (char c in content)
                 {
-                    if (Convert.ToInt32(c) >= 48 && Convert.ToInt32(c) <= 57)
+                    code = Convert.ToInt32(c);
+
+                    if (code >= 48 && code <= 57)
                     {
                         builder.Append(c);
                     }
-                    else if (Convert.ToInt32(c) == ASCii.HYPHEN)
+                    else if (code == ASCii.HYPHEN)
                     {
                         if (builder.Length == 0)
                         {
@@ -32,7 +36,7 @@ namespace Game
                 }
 
                 if (builder.Length > 0 && int.TryParse(builder.ToString(), out int value))
-                { 
+                {
                     return value;
                 }
                 return -1;
@@ -44,15 +48,17 @@ namespace Game
 
                 foreach (char c in content)
                 {
-                    if (Convert.ToInt32(c) >= ASCii.NUMBER0 && Convert.ToInt32(c) <= ASCii.NUMBER9)
+                    code = Convert.ToInt32(c);
+
+                    if (code >= ASCii.NUMBER0 && code <= ASCii.NUMBER9)
                     {
                         builder.Append(c);
                     }
-                    else if (Convert.ToInt32(c) == ASCii.PERIOD)
+                    else if (code == ASCii.PERIOD)
                     {
                         builder.Append(c);
                     }
-                    else if (Convert.ToInt32(c) == ASCii.HYPHEN)
+                    else if (code == ASCii.HYPHEN)
                     {
                         if (builder.Length == 0)
                         {
