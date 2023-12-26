e440f9566c925b1686430c702625ddd6527e7ee7
dongsheng <do_she_ng@163.com>
2023-12-26 18:33:45 +0800

    学习

---
+++ Assets/Scripts/Test/TestCollider.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Game.Test
+{
+    internal class TestCollider : MonoBehaviour
+    {
+        [SerializeField] private new Rigidbody rigidbody;
+
+        [SerializeField] private Vector3 vector;
+
+        [SerializeField] private Vector3 position;
+
+        [SerializeField] private bool move;
+
+        private void Update()
+        {
+            vector.x = Input.GetAxis("Horizontal");
+
+            vector.z = Input.GetAxis("Vertical");
+
+            rigidbody.AddForce(vector, ForceMode.Force);
+        }
+
+        private void FixedUpdate()
+        {
+            if (!move) return;
+
+            rigidbody.MovePosition(position);
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            Debuger.LogError(Author.Test, "Enter");
+        }
+
+        private void OnCollisionStay(Collision collision)
+        {
+            Debuger.Log(Author.Test, "Stay");
+        }
+
+        private void OnCollisionExit(Collision collision)
+        {
+            Debuger.LogWarning(Author.Test, "Exit");
+        }
+    }
+}
--- Assets/Scripts/Utility/Utility.Zip.cs
+++ Assets/Scripts/Utility/Utility.Zip.cs
@@ -1,3 +1,5 @@
+using ICSharpCode.SharpZipLib.Checksum;
+using ICSharpCode.SharpZipLib.Zip;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -5,6 +7,7 @@ using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace Game
 {
@@ -12,14 +15,95 @@ namespace Game
     {
         public static class Zip
         {
-            public static void CreateFromDirectory(string src, string dst)
+            public static void Compress(string src, string dst, int level = 5, string password = null)
             {
-                ZipFile.CreateFromDirectory(src, dst);
+                if (File.Exists(src))
+                {
+                    CompressFile(src, dst, level, password);
+                }
+                else if (Directory.Exists(src))
+                {
+                    CompressDirectory(src, dst, level, password);
+                }
+                else
+                {
+                    Debuger.LogError(Author.File, $"File {src} not exist!");
+                }
             }
 
-            public static void ExtractToDirectory(string src, string dst)
+            public static void CompressFile(string src, string dst, int level = 5, string password = null)
             {
-                ZipFile.ExtractToDirectory(src, dst);
+                byte[] buffer = File.ReadAllBytes(src);
+
+                Document.CreateDirectory(Path.GetDirectoryName(dst));
+
+                using (var stream = new FileStream(dst, FileMode.OpenOrCreate))
+                {
+                    using (var zip = new ZipOutputStream(stream))
+                    {
+                        if (!string.IsNullOrEmpty(password))
+                        {
+                            zip.Password = password;
+                        } 
+                        zip.PutNextEntry(new ZipEntry(Path.GetFileName(src)));
+                        zip.SetLevel(level);
+                        zip.Write(buffer, 0, buffer.Length);
+                    }
+                }
+            }
+
+            public static void CompressDirectory(string src, string dst, int level = 5, string password = null)
+            {
+                //Crc32 crc = new Crc32();
+
+                //string[] filenames = Directory.GetFileSystemEntries(strDirectory);
+
+                //foreach (string file in filenames)
+                //{
+                //    if (Directory.Exists(file))
+                //    {
+                //        string pPath = parentPath;
+                //        pPath += file.Substring(file.LastIndexOf("/") + 1);
+                //        pPath += "/";
+                //        ZipSetp(file, s, pPath);
+                //    }
+                //    else
+                //    {
+                //        using (FileStream fs = File.OpenRead(file))
+                //        {
+                //            byte[] buffer = new byte[fs.Length];
+                //            fs.Read(buffer, 0, buffer.Length);
+
+                //            string fileName = parentPath + file.Substring(file.LastIndexOf("/") + 1);
+                //            ZipEntry entry = new ZipEntry(fileName);
+
+                //            entry.DateTime = DateTime.Now;
+                //            entry.Size = fs.Length;
+
+                //            fs.Close();
+
+                //            crc.Reset();
+                //            crc.Update(buffer);
+
+                //            entry.Crc = crc.Value;
+                //            s.PutNextEntry(entry);
+
+                //            s.Write(buffer, 0, buffer.Length);
+                //        }
+                //    }
+                //}
+            }
+
+            public static void Uncompress(string src, string dst, string password)
+            {
+                if (File.Exists(src))
+                {
+
+                }
+                else
+                {
+                    Debuger.LogError(Author.File, $"File {src} not exist!");
+                }
             }
         }
     }
