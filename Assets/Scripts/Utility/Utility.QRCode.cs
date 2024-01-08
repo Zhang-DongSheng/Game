70939184b6532ee418b0d65c5d6950a50718ee1d
dongsheng <do_she_ng@163.com>
2024-01-08 18:49:32 +0800

    二维码

---
+++ Assets/Scripts/Utility/Utility.QRCode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static partial class Utility
+{
+	/// <summary>
+	/// 二维码
+	/// </summary>
+	public static class QRCode
+	{
+
+	}
+}
