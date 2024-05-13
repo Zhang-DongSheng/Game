29755fd199d50c3e039eba33569d8fc5d180618b
dongsheng <do_she_ng@163.com>
2024-05-13 19:13:54 +0800

    资源重命名

---
+++ Assets/Scripts/UI/Hall/Chat/UIChat.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.UI
+{
+    public class UIChat : UIBase
+    {
+        protected override void OnUpdate(float delta)
+        {
+            
+        }
+
+        public override void Refresh(UIParameter paramter)
+        {
+            
+        }
+    }
+}
