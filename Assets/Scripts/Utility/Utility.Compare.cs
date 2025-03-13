0f22b0e6482786a0a6d94d609a2152ce093127fb
dongsheng <do_she_ng@163.com>
2025-03-13 11:28:22 +0800

    比较

---
+++ Assets/Scripts/Utility/Utility.Compare.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace Game
+{
+    public static partial class Utility
+    {
+        /// <summary>
+        /// 比较
+        /// </summary>
+        public static class Compare
+        {
+            public static bool List<T>(IList<T> list, IList<T> other)
+            {
+                if (list == null || other == null) return false;
+
+                if (list.Count != other.Count) return false;
+
+                var clone = new List<T>(other);
+
+                int count = list.Count;
+
+                for (int i = 0; i < count; i++)
+                {
+                    var index = clone.FindIndex(x => x.Equals(list[i]));
+
+                    if (index > -1)
+                    {
+                        clone.RemoveAt(index);
+                    }
+                }
+                return clone.Count == 0;
+            }
+        }
+    }
+}
