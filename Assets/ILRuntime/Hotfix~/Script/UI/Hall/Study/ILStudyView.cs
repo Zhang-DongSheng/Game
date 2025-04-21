9b8037c3fc4f2c8e6c11284b857ce3f1c388cd71
dongsheng <do_she_ng@163.com>
2025-04-21 14:46:53 +0800

    IL热更测试

--- Assets/ILRuntime/Hotfix~/Script/UI/Hall/Study/ILStudyView.cs
+++ Assets/ILRuntime/Hotfix~/Script/UI/Hall/Study/ILStudyView.cs
@@ -5,6 +5,19 @@ namespace ILRuntime.Game.UI
 {
     public class ILStudyView : ILViewBase
     {
-        
+        public override void Init(Transform transform)
+        {
+            Debuger.Log("ILStudyView Init ...");
+        }
+
+        public override void Refresh(UIParameter paramter)
+        {
+            Debuger.Log("ILStudyView Refresh ...");
+        }
+
+        public override void Release()
+        {
+            Debuger.Log("ILStudyView Release ...");
+        }
     }
 }
