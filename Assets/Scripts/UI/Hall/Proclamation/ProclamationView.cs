4c6d280bfafc8ca5594f4c5153d4b459c2245ec0
dongsheng <do_she_ng@163.com>
2025-10-16 11:46:51 +0800

    学习Html解析工具AngleSharp

--- Assets/Scripts/UI/Hall/Proclamation/ProclamationView.cs
+++ Assets/Scripts/UI/Hall/Proclamation/ProclamationView.cs
@@ -1,13 +1,17 @@
 using Game.Logic;
+using System;
 using System.Collections;
-using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 
 namespace Game.UI
 {
     public class ProclamationView : ViewBase
     {
+        [SerializeField] private string url;
+
         [SerializeField] private Text txtContent;
 
         [SerializeField] private Button btnConfirm;
@@ -19,7 +23,45 @@ namespace Game.UI
 
         public override void Refresh(UIParameter paramter)
         {
-            
+            StartCoroutine(LoadContent());
+        }
+
+        private IEnumerator LoadContent()
+        { 
+            var request = UnityWebRequest.Get(url);
+
+            yield return request.SendWebRequest();
+
+            if (request.result == UnityWebRequest.Result.Success)
+            {
+                var steam = StreamFromString(request.downloadHandler.text);
+
+                var document = steam.ToHtmlDocument();
+                // 查找页面中的标题
+                var qs = document.QuerySelector("body");
+
+                txtContent.text = qs.TextContent;
+            }
+            else
+            {
+                Debug.LogError($"Failed to load proclamation: {request.error}");
+            }
+        }
+
+        public static Stream StreamFromBytes(Byte[] content)
+        {
+            var stream = new MemoryStream(content) { Position = 0 };
+            return stream;
+        }
+
+        public static Stream StreamFromString(String s)
+        {
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(s);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
         }
 
         public override void Exit()
